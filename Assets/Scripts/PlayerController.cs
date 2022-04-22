e82d4021e23ffa413a7d4d69532c044bb1a60636
Daniel Ribeiro <daniel@Marcelos-MacBook-Pro.local>
2022-04-22 12:27:45 -0300

    progress

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,6 +7,8 @@ public class PlayerController : MonoBehaviour {
     public Rigidbody2D theRB;
     public float moveSpeed;
 
+    public Animator myAnim;
+
     // Use this for initialization
     void Start () {
         
@@ -15,5 +17,15 @@ public class PlayerController : MonoBehaviour {
     // Update is called once per frame
     void Update () {
         theRB.velocity = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")) * moveSpeed;
+
+        myAnim.SetFloat("moveX", theRB.velocity.x);
+        myAnim.SetFloat("moveY", theRB.velocity.y);
+
+        if(Input.GetAxisRaw("Horizontal") == 1 || Input.GetAxisRaw("Horizontal") == -1 || Input.GetAxisRaw("Vertical") == 1 || Input.GetAxisRaw("Vertical") == -1) 
+        {
+            myAnim.SetFloat("lastMoveX", Input.GetAxisRaw("Horizontal"));
+            myAnim.SetFloat("lastMoveY", Input.GetAxisRaw("Vertical"));
+        }
+           
     }
 }
